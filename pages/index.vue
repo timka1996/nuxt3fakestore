1b4d8716baf78d1710211463d46b5f71e0538e3b
unknown <valievtemurbek@gmail.com>
2023-08-07 16:30:15 +0500

    first git commit -m first

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae tempora est omnis
+             rerum iure eveniet, nihil quos temporibus 
+            voluptate veritatis expedita iste quisquam consequatur possimus facere laboriosam nesciunt
+             qui inventore quia libero dolorem maxime modi adipisci. Commodi, saepe unde reprehenderit 
+             delectus sequi facere dicta voluptatem! Perspiciatis, quasi asperiores? Repellendus,
+              voluptas.
+        </p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+       <ProductDetails :product='product'/>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+const uri = 'https://fakestoreapi.com/products/' + id
+
+const { data: product } = await useFetch(uri)
+
+    if(!product.value){
+        throw createError({statusCode:404, statusMessage: 'Product not found'})
+    }
+
+ definePageMeta({
+        layout: "products"
+    })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
